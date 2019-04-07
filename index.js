6999ad510c97a5e74e53b9ff01da30135be24a2c
Oleg Shcherbachenko <oleg.shcherbachenko@rolique.io>
2019-04-07 22:02:57 +0300

    added heroku

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const {mongoURL} = require(`${__dirname}/config/keys`);
-const shoppingItems = require(`${__dirname}/routes/api/shoppingItems`);
+const path = require('path');
+const { mongoURL } = require(`./config/keys`);
+const shoppingItems = require(`./routes/api/shoppingItems`);
 
 const app = express();
 
 app.use(bodyParser.json());
+// define api routing here
 app.use('/api/shopping-items', shoppingItems);
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// send static assets
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
 
 mongoose
-  .connect(mongoURL, {useNewUrlParser: true})
+  .connect(mongoURL, { useNewUrlParser: true })
   .then(() => console.log('Connected MongoDB'))
-  .catch((err) => console.error(err))
+  .catch((err) => console.error(err));
 
 const port = process.env.PORT || 5000;
 
